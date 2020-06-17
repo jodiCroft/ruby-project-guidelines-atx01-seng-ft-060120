373345e49a67270a19b49625a89217f3a4254c25
github username <github email address>
2020-06-17 14:25:06 -0500

    All attrs for breeds

--- lib/dog_api.rb
+++ lib/dog_api.rb
@@ -19,11 +19,40 @@ class GetBreeds
     breed["name"]  
   end
 end
+
+def breed_temperament
+    # we use the JSON library to parse the API response into nicely formatted JSON
+      all_breeds = JSON.parse(self.get_all_breeds)
+      all_breeds.collect do |breed|
+        breed["temperament"]  
+      end
+    end
+
+def breed_life_span
+        # we use the JSON library to parse the API response into nicely formatted JSON
+        all_breeds = JSON.parse(self.get_all_breeds)
+        all_breeds.collect do |breed|
+        breed["life_span"]  
+        end
+    end
+
+
+
  
 end
 
-breed_names = GetBreeds.new
-puts breed_names.breed_name
+
+life_span = GetBreeds.new
+puts life_span.breed_life_span
+
+
+# This goes with breed_temperament method
+# breed_temp = GetBreeds.new
+# puts breed_temp.breed_temperament
+
+# This goes with breed_name method
+# breed_names = GetBreeds.new
+# puts breed_names.breed_name
  
 # all_breeds = GetBreeds.new.get_all_breeds
 # puts all_breeds
